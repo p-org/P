4845854ab0a3d72ff06b06216c093f66bc1df85b
ankushd <ankushdesai@gmail.com>
2014-09-26 21:17:58 -0700

    actions on defaults allowed now

--- Bld/PlangBuild/PlangBuild/SourceDownloader.cs
+++ Bld/PlangBuild/PlangBuild/SourceDownloader.cs
@@ -37,7 +37,7 @@
 
         private static readonly Tuple<string, string, string, string>[] Versions = new Tuple<string, string, string, string>[] 
         {
-            new Tuple<string, string, string, string>("zing", "3fe727de291673cf87cd310e174b0f23ab1ede4f", "..\\..\\..\\..\\..\\Ext\\Zing\\Zing_.zip", "..\\..\\..\\..\\..\\Ext\\Zing\\Zing_\\"),
+            new Tuple<string, string, string, string>("zing", "3ffebfcb797fb957fcb6fb0d0592da01b2389276", "..\\..\\..\\..\\..\\Ext\\Zing\\Zing_.zip", "..\\..\\..\\..\\..\\Ext\\Zing\\Zing_\\"),
             new Tuple<string, string, string, string>("formula", "1b41678966e1d0385f3f9d4107f67e2f15929e86", "..\\..\\..\\..\\..\\Ext\\Formula\\Formula_.zip", "..\\..\\..\\..\\..\\Ext\\Formula\\Formula_\\"),
         };
        

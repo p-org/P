7f992f90dec20b9abe8b700ab370c940249f62bc
ankushd <ankushdesai@gmail.com>
2014-10-08 21:06:21 -0700

    Added PsTools to the PrtDistributed Resources

---
+++ Src/Prt/Resources/ScriptsForAzure/startPService.ps1
@@ -0,0 +1,12 @@
+﻿$psExec = "./PsExec.exe"
+"Reading the Configuration File"
+
+[xml] $xdox = Get-Content ".\PrtDMConfiguration.xml"
+$nodes = $xdox.configuration.AllNodes.ChildNodes
+foreach($n in $nodes)
+{
+    $psExec -u planguser -p Pldi2015 $n Robocopy $xdox.configuration.DeploymentFolder $xdox.configuration.PServiceFolder /E | Out-File "./startPserviceLog.txt"
+}
+
+
+

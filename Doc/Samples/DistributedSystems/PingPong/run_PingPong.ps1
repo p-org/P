379df45ba1d4c47bc7ba9136b351a6f557676f2b
ankushd <ankushdesai@gmail.com>
2014-10-13 10:22:35 -0700

    Moved models to berkeley repo for editing

--- Doc/Samples/DistributedSystems/PingPong/run_PingPong.ps1
+++
@@ -1,6 +0,0 @@
-﻿echo "Combining all files"
-..\..\..\Scripts\MergeFiles.exe -concate:PingPong.p+NetworkLayer.p+NodeManager.p+CentralServer.p -out:temp1.p
-echo "Merging all global functions"
-..\..\..\Scripts\MergeFiles.exe -merge:output.p -main:temp1.p -model:.\ModelFunctions_Implementation.p 
-echo "Running P"
-python.exe ..\..\..\Scripts\myRunAll.py .\temp output.p

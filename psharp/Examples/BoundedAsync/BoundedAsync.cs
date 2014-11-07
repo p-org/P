84cb680a3a08ed4ebb6aac34e83546cfb127701b
Pantazis Deligiannis <pdeligia@me.com>
2014-11-07 18:19:31 +0000

    psharp - small update to benchmarks

--- psharp/Examples/BoundedAsync/BoundedAsync.cs
+++ psharp/Examples/BoundedAsync/BoundedAsync.cs
@@ -46,7 +46,6 @@ namespace BoundedAsync
     #region Machines
 
     [Main]
-    [Ghost]
     internal class Scheduler : Machine
     {
         private Machine Process1;
@@ -194,7 +193,6 @@ namespace BoundedAsync
         }
     }
 
-    [Ghost]
     internal class Process : Machine
     {
         private int Count;
@@ -339,38 +337,4 @@ namespace BoundedAsync
     }
 
     #endregion
-
-    /// <summary>
-    /// This is an example of usign P#.
-    /// 
-    /// This example implements an asynchronous scheduler communicating
-    /// with a number of processes under a predefined bound.
-    /// </summary>
-    class BoundedAsync
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Registering events to the runtime.\n");
-            Runtime.RegisterNewEvent(typeof(eUnit));
-            Runtime.RegisterNewEvent(typeof(eReq));
-            Runtime.RegisterNewEvent(typeof(eResp));
-            Runtime.RegisterNewEvent(typeof(eDone));
-            Runtime.RegisterNewEvent(typeof(eInit));
-            Runtime.RegisterNewEvent(typeof(eMyCount));
-
-            Console.WriteLine("Registering state machines to the runtime.\n");
-            Runtime.RegisterNewMachine(typeof(Scheduler));
-            Runtime.RegisterNewMachine(typeof(Process));
-
-            Console.WriteLine("Configuring the runtime.\n");
-            Runtime.Options.Mode = Runtime.Mode.BugFinding;
-
-            Console.WriteLine("Starting the runtime.\n");
-            Runtime.Start();
-            Runtime.Wait();
-
-            Console.WriteLine("Performing cleanup.\n");
-            Runtime.Dispose();
-        }
-    }
 }
---
+++ psharp/Examples/BoundedAsync/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.PSharp;
+
+namespace BoundedAsync
+{
+    /// <summary>
+    /// This is an example of usign P#.
+    /// 
+    /// This example implements an asynchronous scheduler communicating
+    /// with a number of processes under a predefined bound.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Registering events to the runtime.\n");
+            Runtime.RegisterNewEvent(typeof(eUnit));
+            Runtime.RegisterNewEvent(typeof(eReq));
+            Runtime.RegisterNewEvent(typeof(eResp));
+            Runtime.RegisterNewEvent(typeof(eDone));
+            Runtime.RegisterNewEvent(typeof(eInit));
+            Runtime.RegisterNewEvent(typeof(eMyCount));
+
+            Console.WriteLine("Registering state machines to the runtime.\n");
+            Runtime.RegisterNewMachine(typeof(Scheduler));
+            Runtime.RegisterNewMachine(typeof(Process));
+
+            Console.WriteLine("Starting the runtime.\n");
+            Runtime.Start();
+            Runtime.Wait();
+
+            Console.WriteLine("Performing cleanup.\n");
+            Runtime.Dispose();
+        }
+    }
+}

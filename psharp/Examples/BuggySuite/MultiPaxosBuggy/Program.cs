07b7c3f3222196c61cad1d72db44c09399b4a7a2
Pantazis Deligiannis <pdeligia@me.com>
2014-11-08 17:10:49 +0000

    psharp - added a buggy benchmark

---
+++ psharp/Examples/BuggySuite/MultiPaxosBuggy/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.PSharp;
+
+namespace MultiPaxosBuggy
+{
+    /// <summary>
+    /// This is an example of usign P#.
+    /// 
+    /// This example implements the MultiPaxos distributed
+    /// concencus algorithm.
+    /// </summary>
+    public class Program
+    {
+        public static void Go()
+        {
+            Runtime.RegisterNewEvent(typeof(ePrepare));
+            Runtime.RegisterNewEvent(typeof(eAccept));
+            Runtime.RegisterNewEvent(typeof(eAgree));
+            Runtime.RegisterNewEvent(typeof(eReject));
+            Runtime.RegisterNewEvent(typeof(eAccepted));
+            Runtime.RegisterNewEvent(typeof(eAllNodes));
+            Runtime.RegisterNewEvent(typeof(eChosen));
+            Runtime.RegisterNewEvent(typeof(eUpdate));
+            Runtime.RegisterNewEvent(typeof(ePing));
+            Runtime.RegisterNewEvent(typeof(eNewLeader));
+            Runtime.RegisterNewEvent(typeof(eMonitorValueChosen));
+            Runtime.RegisterNewEvent(typeof(eMonitorValueProposed));
+            Runtime.RegisterNewEvent(typeof(eMonitorClientSent));
+            Runtime.RegisterNewEvent(typeof(eMonitorProposerSent));
+            Runtime.RegisterNewEvent(typeof(eMonitorProposerChosen));
+            Runtime.RegisterNewEvent(typeof(eLocal));
+            Runtime.RegisterNewEvent(typeof(eSuccess));
+            Runtime.RegisterNewEvent(typeof(eGoPropose));
+            Runtime.RegisterNewEvent(typeof(eStartTimer));
+            Runtime.RegisterNewEvent(typeof(eCancelTimer));
+            Runtime.RegisterNewEvent(typeof(eCancelTimerSuccess));
+            Runtime.RegisterNewEvent(typeof(eTimeout));
+            Runtime.RegisterNewEvent(typeof(eStop));
+
+            Runtime.RegisterNewMachine(typeof(GodMachine));
+            Runtime.RegisterNewMachine(typeof(Client));
+            Runtime.RegisterNewMachine(typeof(PaxosNode));
+            Runtime.RegisterNewMachine(typeof(LeaderElection));
+            Runtime.RegisterNewMachine(typeof(Timer));
+            Runtime.RegisterNewMachine(typeof(PaxosInvariantMonitor));
+            Runtime.RegisterNewMachine(typeof(ValidityCheckMonitor));
+
+            Runtime.Start();
+            Runtime.Wait();
+            Runtime.Dispose();
+        }
+
+        static void Main(string[] args)
+        {
+            Runtime.Test(
+                () =>
+                {
+                    Go();
+                },
+                10,
+                true,
+                Runtime.SchedulingType.Random,
+                false);
+        }
+    }
+
+    public class ChessTest
+    {
+        public static bool Run()
+        {
+            Program.Go();
+            return true;
+        }
+    }
+}

af236929be29d884ab7345b566ebcc4dbf0e1fa3
ankushd <ankushdesai@gmail.com>
2014-12-15 21:41:33 -0800

    Updated Zing version and also delaying scheduler instrumentation in generated code

--- Bld/PlangBuild/PlangBuild/SourceDownloader.cs
+++ Bld/PlangBuild/PlangBuild/SourceDownloader.cs
@@ -37,7 +37,7 @@
 
         private static readonly Tuple<string, string, string, string>[] Versions = new Tuple<string, string, string, string>[] 
         {
-            new Tuple<string, string, string, string>("zing", "d385746b7d2bd7c948095815d64ba9f5668023c7", "..\\..\\..\\..\\..\\Ext\\Zing\\Zing_.zip", "..\\..\\..\\..\\..\\Ext\\Zing\\Zing_\\"),
+            new Tuple<string, string, string, string>("zing", "8b903804873d9b7f1296ccc9a730cdf37395e811", "..\\..\\..\\..\\..\\Ext\\Zing\\Zing_.zip", "..\\..\\..\\..\\..\\Ext\\Zing\\Zing_\\"),
             new Tuple<string, string, string, string>("formula", "aa4cf15c2670c57d0f7ac86888aa17dbe6e47cd0", "..\\..\\..\\..\\..\\Ext\\Formula\\Formula_.zip", "..\\..\\..\\..\\..\\Ext\\Formula\\Formula_\\"),
         };
        

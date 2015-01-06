bdc6eb6f4435ec034642cf32c1bf759628e7ff99
Ella <ellab@microsoft.com>
2015-01-05 17:35:37 -0800

    added /doNotErase parameter to testconfigPc.txt (needed for runtime testing)

---
+++ Tst/addPar.pl
@@ -0,0 +1,32 @@
+use File::Find; 
+
+sub wanted 
+{
+    if ($_ eq "testconfigPc.txt")
+    {
+        open(IN,"testconfigPc.txt") or die "Can't open testconfigPc.txt";
+        open(OUT,">","testconfigPc.out") or die "Can't open testconfigPc.out"; 
+    while (<IN>)
+    {
+       @lines = split(":");
+
+       if ($lines[0] eq "acc")
+       {
+          print OUT "argPc: /doNotErase\n";
+          print OUT "acc: .\\\n";
+       }
+       else
+       {
+          print OUT $_; 
+       }
+    }
+
+    close IN;
+    close OUT;
+    system("move testconfigPc.out testconfigPc.txt"); 
+   }    
+
+}
+
+
+find(\&wanted,$ARGV[0]); 

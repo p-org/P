5e2b373d46e3ad541e66b80be09948595408c9bf
Ella <ellab@microsoft.com>
2015-01-09 14:04:16 -0800

    Runtime template files added to runtime tester dir PrtTester; testP.bat updated for runtime

---
+++ Tst/PrtTester/RuntimeTemplates/stubs.c
@@ -0,0 +1,15 @@
+
+  #ifndef P_PROGRAM_H
+    #define P_PROGRAM_H
+    #include "PrtWinUser.h"
+    #include "PrtExecution.h"
+    void P_DTOR_Program_IMPL(PRT_MACHINEINST *context)
+    {
+    }
+
+    void P_CTOR_Program_IMPL(PRT_MACHINEINST *context, PRT_VALUE *value)
+    {
+    }
+
+  #endif
+  

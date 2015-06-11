fd289ed524c9bc21ab7333f52b80a51e5e53a3ff
qadeer <qadeer@microsoft.com>
2015-06-11 08:29:49 -0700

    updated test tool to directly include Compiler.csproj

--- Tst/testP.bat
+++ Tst/testP.bat
@@ -12,19 +12,19 @@ if exist "%WinDir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" (
   exit /B 1)
 
 echo Bootstrapping the test utility...
-%bldexe%  .\Tools\RunPTool\RunPTool.csproj /t:Clean /p:Configuration=Debug /p:Platform=AnyCPU /verbosity:quiet /nologo
+%bldexe%  .\Tools\RunPTool\RunPTool.csproj /t:Clean /p:Configuration=Debug /p:Platform=x86 /verbosity:quiet /nologo
 if %ERRORLEVEL% neq 0 (
   echo Could not clean test utility.
   exit /B 1
 )
 
-%bldexe%  .\Tools\RunPTool\RunPTool.csproj /t:Build /p:Configuration=Debug /p:Platform=AnyCPU /verbosity:quiet /nologo
+%bldexe%  .\Tools\RunPTool\RunPTool.csproj /t:Build /p:Configuration=Debug /p:Platform=x86 /verbosity:quiet /nologo
 if %ERRORLEVEL% neq 0 (
   echo Could not compile test utility.
   exit /B 1
 )
 
-.\Tools\RunPTool\bin\Debug\RunPTool.exe %1 %2 %3
+.\Tools\RunPTool\bin\x86\Debug\RunPTool.exe %1 %2 %3
 if %ERRORLEVEL% neq 0 (
   echo Tests failed.
   exit /B 1

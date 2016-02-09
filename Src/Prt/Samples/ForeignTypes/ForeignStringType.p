932b378bccc86eb2d0b9d544afabfba1d8e350f0
ankushd <ankushdesai@gmail.com>
2016-02-08 16:29:46 -0800

    updated the foreign type sample

--- Src/Prt/Samples/ForeignTypes/ForeignStringType.p
+++ Src/Prt/Samples/ForeignTypes/ForeignStringType.p
@@ -1,5 +1,4 @@
 type StringType;
-type PointerType;
 
 event sendback : (machine, any);
 event getback : any;
@@ -7,24 +6,16 @@ event getback : any;
 main machine TestMachine
 {
 	var someStringV : StringType;
-	var somePointerV : PointerType;
 	var fMachine: machine;
 	start state Init {
 
 		entry {
 			fMachine = new ForwardingMachine();
 			someStringV = GetPassword();
-			somePointerV = GetPointerToSomething(someStringV);
 			send fMachine, sendback, (this, someStringV);
 			receive {
 				case getback: (payload: any) {
 					assert((payload as StringType) == someStringV);
-				}
-			}
-			send fMachine, sendback, (this, somePointerV);
-			receive {
-				case getback: (payload: any) {
-					assert((payload as PointerType) == somePointerV);
 				}
 			}
 		}
@@ -35,17 +26,6 @@ main machine TestMachine
 		return fresh(StringType);
 	}
 
-	model fun GetPointerToSomething(password: StringType) : PointerType
-	{
-		if(password == someStringV)
-		{
-			return fresh(PointerType);
-		}
-		else
-		{
-			assert(false);
-		}
-	}
 }
 
 machine ForwardingMachine {
--- Src/Prt/Samples/ForeignTypes/ForeignTypes.c
+++ Src/Prt/Samples/ForeignTypes/ForeignTypes.c
@@ -2,18 +2,41 @@
 
 PRT_UINT64 UserMkDefaultForeignValue(_In_ PRT_UINT16 typeTag)
 {
-	PRT_STRING str = PrtMalloc(sizeof(PRT_CHAR) * 100);
-	sprintf_s(str, 100, "xyx$12");
-	return (PRT_UINT64)str;
+	PRT_STRING str = NULL;
+	switch (typeTag)
+	{
+	case P_FORGN_TYPE_StringType:
+		str = PrtMalloc(sizeof(PRT_CHAR) * 100);
+		sprintf_s(str, 100, "xyx$12");
+		return (PRT_UINT64)str;
+	default:
+		return (PRT_UINT64)NULL;
+	};
 }
 
 PRT_UINT64 UserCloneForeignValue(_In_ PRT_UINT16 typeTag, _In_ PRT_UINT64 frgnVal)
 {
-	return frgnVal;
+	PRT_STRING str = NULL;
+	switch (typeTag)
+	{
+	case P_FORGN_TYPE_StringType:
+		str = PrtMalloc(sizeof(PRT_CHAR) * 100);
+		sprintf_s(str, 100, (PRT_STRING)frgnVal);
+		return (PRT_UINT64)str;
+	default:
+		return (PRT_UINT64)NULL;
+	};
 }
 
 void UserFreeForeignValue(_In_ PRT_UINT16 typeTag, _Inout_ PRT_UINT64 frgnVal)
 {
+	switch (typeTag)
+	{
+	case P_FORGN_TYPE_StringType:
+		PrtFree((PRT_STRING)frgnVal);;
+	default:
+		break;
+	};
 }
 
 PRT_UINT32 UserHashForeignValue(_In_ PRT_UINT16 typeTag, _In_ PRT_UINT64 frgnVal)
@@ -26,9 +49,6 @@ PRT_STRING UserToStringForeignValue(_In_ PRT_UINT16 typeTag, _In_ PRT_UINT64 frg
 	PRT_STRING str = PrtMalloc(sizeof(PRT_CHAR) * 100);
 	switch (typeTag)
 	{
-	case P_FORGN_TYPE_PointerType:
-		sprintf_s(str, 100, "Pointer : %d", frgnVal);
-		break;
 	case P_FORGN_TYPE_StringType:
 		sprintf_s(str, 100, "String : %s", frgnVal);
 		break;
@@ -46,8 +66,6 @@ PRT_BOOLEAN UserIsEqualForeignValue(_In_ PRT_UINT16 typeTag1, _In_ PRT_UINT64 fr
 	{
 		switch (typeTag1)
 		{
-		case P_FORGN_TYPE_PointerType:
-			return frgnVal1 == frgnVal2;
 		case P_FORGN_TYPE_StringType:
 			return (strcmp((PRT_STRING)frgnVal1, (PRT_STRING)frgnVal2) == 0);
 		default:
@@ -66,17 +84,6 @@ PRT_VALUE *P_FUN_TestMachine_GetPassword_IMPL(PRT_MACHINEINST *context)
 	PrtPopFrame(p_tmp_mach_priv, &p_tmp_frame);
 	//remm to free the frame
 	PrtFreeLocals(p_tmp_mach_priv, &p_tmp_frame);
-	return PrtMkDefaultValue(P_GEND_TYPE_StringType);
-}
 
-PRT_VALUE *P_FUN_TestMachine_GetPointerToSomething_IMPL(PRT_MACHINEINST *context)
-{
-	PRT_MACHINEINST_PRIV *p_tmp_mach_priv = (PRT_MACHINEINST_PRIV *)context;
-	PRT_VALUE *p_tmp_ret = NULL;
-	PRT_FUNSTACK_INFO p_tmp_frame;
-	//remm to pop frame
-	PrtPopFrame(p_tmp_mach_priv, &p_tmp_frame);
-	//remm to free the frame
-	PrtFreeLocals(p_tmp_mach_priv, &p_tmp_frame);
-	return PrtMkDefaultValue(P_GEND_TYPE_PointerType);
+	return PrtMkDefaultValue(P_GEND_TYPE_StringType);
 }

94a527659eba69b879edcee9c312642baa3b93f3
Shaz Qadeer <qadeer@microsoft.com>
2016-02-27 08:14:14 -0800

    added option for ouptut file names

--- Src/Pc/Compiler/CommandLineOptions.cs
+++ Src/Pc/Compiler/CommandLineOptions.cs
@@ -17,6 +17,7 @@
         public bool analyzeOnly;
         public LivenessOption liveness;
         public string outputDir;
+        public string outFileName;
         public bool outputFormula;
         public bool erase;
         public bool shortFileNames;

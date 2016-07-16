166de73e44fa007836a6bc8c10c1f408873158df
Ankush Desai <ankushdesai@gmail.com>
2016-07-15 23:18:11 -0700

    removed the warning in PrtDistLogging

--- Src/PrtDist/Core/PrtDist.h
+++ Src/PrtDist/Core/PrtDist.h
@@ -12,8 +12,11 @@ extern PRT_INT64 sendMessageSeqNumber;
 //Functions to help logging
 void
 PrtDistSMExceptionHandler(
-__in PRT_STATUS exception,
-__in PRT_MACHINEINST* vcontext
+	__in PRT_STATUS exception,
+	__in PRT_MACHINEINST* sender,
+	__in PRT_MACHINEINST* receiver,
+	__in PRT_VALUE* event,
+	__in PRT_VALUE* payload
 );
 
 // Function to open log file in a given directory

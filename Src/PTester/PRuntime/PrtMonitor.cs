1e13e2933432f20b02ea19e159e877942eacb37c
Ankush Desai <ankushdesai@gmail.com>
2016-08-11 15:29:16 -0700

    Added P monitor

---
+++ Src/PTester/PRuntime/PrtMonitor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P.PRuntime
+{
+    public abstract class PrtMonitor : ICloneable
+    {
+        public abstract bool IsHot
+        {
+            get;
+        }
+
+        public abstract PrtState StartState
+        {
+            get;
+        }
+
+        public abstract void Invoke();
+
+        public object Clone()
+        {
+            return null;
+        }
+    }
+}

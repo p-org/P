d6c7d679a7e91dddee36d1ccbde9c4413a937d52
lovettchris <clovett@microsoft.com>
2016-09-09 17:57:32 -0700

    add elapsed time output from Build.bat

--- Bld/build.bat
+++ Bld/build.bat
@@ -132,7 +132,36 @@ echo msbuild FormulaCodeGeneratorTask /p:Platform=%FormulaCodeGeneratorTaskPlatf
 msbuild  ext\Formula\src\Extensions\FormulaCodeGeneratorTask\FormulaCodeGeneratorTask.csproj /p:Platform=%FormulaCodeGeneratorTaskPlatform% /p:Configuration=%Configuration%
 
 if "%CleanOnly%"=="true" goto :exit
+
+call :StartTimer
 msbuild P.sln /p:Platform=%Platform% /p:Configuration=%Configuration% /p:SOLVER=NOSOLVER
+call :StopTimer
+call :DisplayTimerResult
+
+goto :exit
+
+:StartTimer
+:: Store start time
+set StartTIME=%TIME%
+for /f "usebackq tokens=1-4 delims=:., " %%f in (`echo %StartTIME: =0%`) do set /a Start100S=1%%f*360000+1%%g*6000+1%%h*100+1%%i-36610100
+goto :EOF
+
+:StopTimer
+:: Get the end time
+set StopTIME=%TIME%
+for /f "usebackq tokens=1-4 delims=:., " %%f in (`echo %StopTIME: =0%`) do set /a Stop100S=1%%f*360000+1%%g*6000+1%%h*100+1%%i-36610100
+:: Test midnight rollover. If so, add 1 day=8640000 1/100ths secs
+if %Stop100S% LSS %Start100S% set /a Stop100S+=8640000
+set /a TookTime=%Stop100S%-%Start100S%
+set TookTimePadded=0%TookTime%
+goto :EOF
+
+:DisplayTimerResult
+:: Show timer start/stop/delta
+echo Started: %StartTime%
+echo Stopped: %StopTime%
+echo Elapsed: %TookTime:~0,-2%.%TookTimePadded:~-2% seconds
+goto :EOF
 
 :exit
 popd

e5f3d41fe0ab0af765601e88761e7097a187f042
Jianqiao Yang <jq.yang@berkeley.edu>
2016-09-09 17:54:37 -0700

    fix build.sh for copying over Zing files; Need more work to make Zing work on linux

--- Bld/build.sh
+++ Bld/build.sh
@@ -38,33 +38,32 @@ if [ $? -ne  0 ]; then
     exit 2
 fi
 
-set BinaryDrop=../../Bld/Drops/$Configuration/$Platform/Binaries
+BinaryDrop=../../Bld/Drops/$Configuration/$Platform/Binaries
 
 if [ ! -f $BinaryDrop ]; then
     mkdir -p $BinaryDrop
 fi
 
-set filesToCopy="zc/bin/$Platform/Release/zc.exe
-             ZingExplorer/bin/$Platform/Release/ZingExplorer.dll
-             Zinger/bin/$Platform/Release/Zinger.exe
-             Microsoft.Zing/bin/$Platform/Release/Microsoft.Zing.dll
-             Microsoft.Zing.Runtime/bin/$Platform/Release/Microsoft.Zing.Runtime.dll
-             Microsoft.Zing/bin/$Platform/Release/Microsoft.Comega.dll
-             Microsoft.Zing/bin/$Platform/Release/Microsoft.Comega.Runtime.dll
+filesToCopy="zc/bin/$Platform/$Configuration/zc.exe
+             ZingExplorer/bin/$Platform/$Configuration/ZingExplorer.dll
+             Zinger/bin/$Platform/$Configuration/Zinger.exe
+             Microsoft.Zing/bin/$Platform/$Configuration/Microsoft.Zing.dll
+             Microsoft.Zing.Runtime/bin/$Platform/$Configuration/Microsoft.Zing.Runtime.dll
+             Microsoft.Zing/bin/$Platform/$Configuration/Microsoft.Comega.dll
+             Microsoft.Zing/bin/$Platform/$Configuration/Microsoft.Comega.Runtime.dll
              Resources/external/CCI/System.Compiler.dll
              Resources/external/CCI/System.Compiler.Framework.dll
              Resources/external/CCI/System.Compiler.Runtime.dll
-             DelayingSchedulers/CustomDelayingScheduler/bin/$Platform/Release/CustomDelayingScheduler.dll
-             DelayingSchedulers/RandomDelayingScheduler/bin/$Platform/Release/RandomDelayingScheduler.dll
-             DelayingSchedulers/RoundRobinDelayingScheduler/bin/$Platform/Release/RoundRobinDelayingScheduler.dll
-             DelayingSchedulers/RunToCompletionDelayingScheduler/bin/$Platform/Release/RunToCompletionDelayingScheduler.dll" 
-
+             DelayingSchedulers/CustomDelayingScheduler/bin/$Platform/$Configuration/CustomDelayingScheduler.dll
+             DelayingSchedulers/RandomDelayingScheduler/bin/$Platform/$Configuration/RandomDelayingScheduler.dll
+             DelayingSchedulers/RoundRobinDelayingScheduler/bin/$Platform/$Configuration/RoundRobinDelayingScheduler.dll
+             DelayingSchedulers/RunToCompletionDelayingScheduler/bin/$Platform/$Configuration/RunToCompletionDelayingScheduler.dll" 
 
 for i in $filesToCopy
 do
     cp $i $BinaryDrop
 done
-   
+
 cd ../..
 
 # This code fixes a problem in MIDL compile by forcing recompile of these files for each configuration.

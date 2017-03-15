a9c443f15d7c2baa3d55dbf91889876b1e06174d
Ankush Desai <ankushdesai@gmail.com>
2017-03-15 11:45:32 -0700

    intermediate commit for refinement

---
+++ Src/PTester/PTester/Refinement.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using P.Runtime;
+
+namespace P.Tester
+{
+    /// <summary>
+    /// Visible actions of a trace
+    /// </summary>
+    public abstract class VisibleAction
+    {
+
+    }
+
+    public class SendAction
+    {
+        Tuple<string, int> target;
+        PrtEventValue ev;
+        PrtValue payload;
+
+        public SendAction(Tuple<string, int> t, PrtEventValue e, PrtValue v)
+        {
+            target = new Tuple<string, int>(t.Item1, t.Item2);
+            ev = e;
+            payload = v.Clone();
+        }
+        public override bool Equals(object obj)
+        {
+            var sendAct = obj as SendAction;
+            if(sendAct == null)
+            {
+                return false;
+            }
+            else
+            {
+                return sendAct.target.Item1 == this.target.Item1 &&
+                    sendAct.target.Item2 == this.target.Item2 &&
+                    sendAct.ev == this.ev &&
+                    sendAct.payload == this.payload;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return target.GetHashCode() ^ ev.GetHashCode() ^ payload.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[({0}, {1}), {2}, {3}]", target.Item1, target.Item2, ev.ToString(), payload.ToString());
+        }
+    }
+
+    public class CreateAction
+    {
+        string interfaceName;
+
+        public CreateAction(string i)
+        {
+            interfaceName = i;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var createAct = obj as CreateAction;
+            if(createAct == null)
+            {
+                return false;
+            }
+            else
+            {
+                return this.interfaceName == createAct.interfaceName;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return interfaceName.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[{0}]", interfaceName);
+        }
+    }
+    /// <summary>
+    /// Sequence of visible actions
+    /// </summary>
+    public class VisibleTrace
+    {
+        List<VisibleAction> trace;
+
+        public VisibleTrace()
+        {
+            trace = new List<VisibleAction>();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var vtrace = obj as VisibleTrace;
+            if(vtrace == null)
+            {
+                return false;
+            }
+            else
+            {
+                var trace2 = vtrace.trace;
+                if (trace.Count != trace2.Count)
+                    return false;
+
+                for (int i = 0; i < trace2.Count(); i++)
+                {
+                    if(trace[i] != trace2[i])
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return trace.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            string ret = "------------\n";
+            foreach(var action in trace)
+            {
+                ret = ret + action.ToString() + "\n";
+            }
+            ret = ret + "------------\n";
+            return ret;
+        }
+    }
+
+    public class RefinementChecking
+    {
+        string LHSModel;
+        string RHSModel;
+
+        public RefinementChecking(string lhs, string rhs)
+        {
+            LHSModel = lhs;
+            RHSModel = rhs;
+        }
+    }
+}

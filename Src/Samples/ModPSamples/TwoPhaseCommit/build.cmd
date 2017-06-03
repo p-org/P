fe51c8a1e55f3e736e76dc0111ee85f87565a9f1
Ankush Desai <ankushdesai@gmail.com>
2017-06-02 17:50:32 -0700

    added the build script

---
+++ Src/Samples/ModPSamples/TwoPhaseCommit/build.cmd
@@ -0,0 +1,27 @@
+set THISDIR=%~dp0
+pushd %THISDIR%
+set pc=..\..\..\..\Bld\drops\Debug\x64\Binaries\pc.exe
+if not exist "%pc%" goto :noP
+
+set pt=..\..\..\..\Bld\drops\Debug\x64\Binaries\pt.exe
+
+%pc% /generate:C# /shared ..\CommonUtilities\Timer.p ..\CommonUtilities\TimerHeader.p /t:Timer.4ml /outputDir:..\CommonUtilities\
+
+if NOT errorlevel 0 goto :eof
+
+%pc% /generate:C# /shared .\TwoPhaseCommitHeader.p .\TwoPhaseCommitClient.p .\TestDrivers.p /r:..\CommonUtilities\Timer.4ml
+
+if NOT errorlevel 0 goto :eof
+
+%pc% /generate:C# /link /shared TestScript.p /r:CoffeeMachine.4ml /r:..\..\Src\Samples\Timer\Timer.4ml
+
+if NOT errorlevel 0 goto :eof
+
+%pt% /psharp Test0.dll
+
+%pt% /psharp Test1.dll
+
+goto :eof
+:noP
+echo please run ..\..\bld\build debug x64
+

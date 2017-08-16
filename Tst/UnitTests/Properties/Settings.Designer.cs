a29e4d736733cc6e136c73bdb017e09c5aa5e3c3
ellab <ellab@microsoft.com>
2017-08-16 11:46:37 -0700

    updates to UnitTests

--- Tst/UnitTests/Properties/Settings.Designer.cs
+++ Tst/UnitTests/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace UnitTests.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool ResetTests {
             get {
                 return ((bool)(this["ResetTests"]));
@@ -43,7 +43,7 @@ namespace UnitTests.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool RunPt {
             get {
                 return ((bool)(this["RunPt"]));
@@ -70,10 +70,19 @@ namespace UnitTests.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool RunAll {
             get {
                 return ((bool)(this["RunAll"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool PtWithPSharp {
+            get {
+                return ((bool)(this["PtWithPSharp"]));
             }
         }
     }

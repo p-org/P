f68d369d959b96c6230cf0be9ab3cf3c688cef92
Alex Reinking <alexander.reinking@yale.edu>
2017-12-07 03:31:35 -0800

    Switched to two-stage parsing strategy to improve ANTLR performance. Parsing is currently the slowest part of the compiler.

--- Src/Pc/Compiler/ANTLRCompiler.cs
+++ Src/Pc/Compiler/ANTLRCompiler.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using Antlr4.Runtime;
+using Antlr4.Runtime.Atn;
 using Antlr4.Runtime.Tree;
 using Microsoft.Formula.API;
 using Microsoft.Pc.Antlr;
@@ -23,14 +24,7 @@ namespace Microsoft.Pc
                 for (var i = 0; i < inputFiles.Length; i++)
                 {
                     FileInfo inputFile = inputFiles[i];
-                    var fileStream = new AntlrFileStream(inputFile.FullName);
-                    var lexer = new PLexer(fileStream);
-                    var tokens = new CommonTokenStream(lexer);
-                    var parser = new PParser(tokens);
-                    parser.RemoveErrorListeners();
-                    parser.AddErrorListener(new PParserErrorListener(inputFile, handler));
-
-                    trees[i] = parser.program();
+                    trees[i] = Parse(handler, inputFile);
                     originalFiles.Put(trees[i], inputFile);
                 }
 
@@ -42,6 +36,32 @@ namespace Microsoft.Pc
             {
                 log.WriteMessage(e.Message, SeverityKind.Error);
                 return false;
+            }
+        }
+
+        private static PParser.ProgramContext Parse(ITranslationErrorHandler handler, FileInfo inputFile)
+        {
+            var fileStream = new AntlrFileStream(inputFile.FullName);
+            var lexer = new PLexer(fileStream);
+            var tokens = new CommonTokenStream(lexer);
+            var parser = new PParser(tokens);
+            parser.RemoveErrorListeners();
+
+            try
+            {
+                // Stage 1: use fast SLL parsing strategy
+                parser.Interpreter.PredictionMode = PredictionMode.Sll;
+                parser.ErrorHandler = new BailErrorStrategy();
+                return parser.program();
+            }
+            catch (Exception e) when (e is RecognitionException || e is OperationCanceledException)
+            {
+                // Stage 2: use slower LL(*) parsing strategy
+                tokens.Reset();
+                parser.AddErrorListener(new PParserErrorListener(inputFile, handler));
+                parser.Interpreter.PredictionMode = PredictionMode.Ll;
+                parser.ErrorHandler = new DefaultErrorStrategy();
+                return parser.program();
             }
         }
 

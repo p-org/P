3c925c0a552dd817f20e27c878909540ecac6e26
Ankush Desai <ankushdesai@gmail.com>
2018-02-14 12:04:01 -0800

    added missing file

---
+++ Src/Pc/CompilerCore/TypeChecker/AST/Declarations/NamedModule.cs
@@ -0,0 +1,16 @@
+﻿using Antlr4.Runtime;
+using Microsoft.Pc.TypeChecker.Types;
+
+namespace Microsoft.Pc.TypeChecker.AST.Declarations
+{
+    public class NamedModule : IPDecl
+    {
+        public NamedModule(string testName, ParserRuleContext sourceNode)
+        {
+
+        }
+
+        public string Name { get; }
+        public ParserRuleContext SourceLocation { get; }
+    }
+}
---
+++ Src/Pc/CompilerCore/TypeChecker/AST/Declarations/RefinementTest.cs
@@ -0,0 +1,16 @@
+﻿using Antlr4.Runtime;
+using Microsoft.Pc.TypeChecker.Types;
+
+namespace Microsoft.Pc.TypeChecker.AST.Declarations
+{
+    public class RefinementTest : IPDecl
+    {
+        public RefinementTest(string testName, ParserRuleContext sourceNode)
+        {
+
+        }
+
+        public string Name { get; }
+        public ParserRuleContext SourceLocation { get; }
+    }
+}

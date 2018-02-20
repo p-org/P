0260152571e59497359c403545c4e9ea2762fe80
Alex Reinking <alex_reinking@berkeley.edu>
2018-02-20 15:44:19 -0800

    configuring NuGet deps on P#

---
+++ Bld/build-core.bat
@@ -0,0 +1,16 @@
+@echo off
+pushd %~dp0
+cd ..
+
+git submodule foreach --recursive git reset --hard
+git submodule foreach --recursive git clean -x -f -d
+git clean -fdx
+
+dotnet build Ext\Formula\Src\Extensions\FormulaCodeGeneratorTask\FormulaCodeGeneratorTask.csproj
+dotnet pack Ext\Formula\Src\Core\Core.csproj --output ..\..\..\..\nupkgs
+dotnet pack Ext\PSharp --output ..\nupkgs
+
+dotnet build P.sln
+
+:exit
+popd

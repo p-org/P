a48328c3246824fc8cfe9ddd6d42d7927e693180
Alex Reinking <alex_reinking@berkeley.edu>
2018-02-20 17:27:18 -0800

    adding new build script

--- Bld/build-core.bat
+++ Bld/build-core.bat
@@ -7,7 +7,7 @@ git submodule foreach --recursive git clean -x -f -d
 git clean -fdx
 
 dotnet build Ext\Formula\Src\Extensions\FormulaCodeGeneratorTask\FormulaCodeGeneratorTask.csproj
-dotnet pack Ext\Formula\Src\Core\Core.csproj --output ..\..\..\..\nupkgs
+dotnet pack Ext\Formula\Src\Core\Core.csproj --output ..\..\..\nupkgs
 dotnet pack Ext\PSharp --output ..\nupkgs
 
 dotnet build P.sln

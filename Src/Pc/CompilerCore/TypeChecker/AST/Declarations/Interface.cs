f18063b9d96faab0703d026037615e522e38cd50
Ankush Desai <ankushdesai@gmail.com>
2018-02-20 22:23:09 -0800

    fixed an initialization error in machine.creates

--- Src/Pc/CompilerCore/TypeChecker/AST/Declarations/Interface.cs
+++ Src/Pc/CompilerCore/TypeChecker/AST/Declarations/Interface.cs
@@ -13,7 +13,7 @@ namespace Microsoft.Pc.TypeChecker.AST.Declarations
 
         public Interface(string name, ParserRuleContext sourceNode)
         {
-            //Debug.Assert(sourceNode is PParser.InterfaceDeclContext);
+            Debug.Assert(sourceNode is PParser.InterfaceDeclContext || sourceNode is PParser.ImplMachineDeclContext);
             Name = name;
             SourceLocation = sourceNode;
         }

237583c527a72d6019619242920d239d54ce7741
Alex Reinking <alex_reinking@berkeley.edu>
2018-06-25 12:34:46 -0700

    Added P# runner

--- Tst/UnitTests/PSharpBackendTests.cs
+++ Tst/UnitTests/PSharpBackendTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using Microsoft.Pc;
+﻿using System.IO;
 using NUnit.Framework;
 using UnitTests.Core;
 using UnitTests.Runners;
@@ -31,60 +27,6 @@ namespace UnitTests
                 new CompileSuccessValidator());
 
             TestAssertions.AssertTestCase(testCase);
-        }
-    }
-
-    internal class PSharpRunner : ICompilerTestRunner
-    {
-        private readonly FileInfo[] sources;
-
-        public PSharpRunner(FileInfo[] sources)
-        {
-            this.sources = sources;
-        }
-
-        public int? RunTest(DirectoryInfo scratchDirectory, out string stdout, out string stderr)
-        {
-            var compiledFiles = DoCompile(scratchDirectory);
-            var psharpPath = Path.Combine(Constants.SolutionDirectory, "Ext", "PSharp", "bin", "Microsoft.PSharp.dll");
-            var args = new[] {$"/r:{psharpPath}"}.Concat(compiledFiles.Select(file => file.Name)).ToArray();
-            return ProcessHelper.RunWithOutput(scratchDirectory.FullName, out stdout, out stderr, FindCsc(), args);
-        }
-
-        private IEnumerable<FileInfo> DoCompile(DirectoryInfo scratchDirectory)
-        {
-            var compiler = new AntlrCompiler();
-            var outputStream = new TestExecutionStream(scratchDirectory);
-            bool success = compiler.Compile(outputStream, new CommandLineOptions
-            {
-                compilerOutput = CompilerOutput.PSharp,
-                inputFileNames = sources.Select(file => file.FullName).ToList(),
-                projectName = "Main"
-            });
-
-            if (!success)
-            {
-                throw new CompilerTestException(TestCaseError.TranslationFailed);
-            }
-
-            return outputStream.OutputFiles;
-        }
-
-        private static string FindCsc()
-        {
-            string[] cscPaths = {
-                @"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\Roslyn\csc.exe",
-                @"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\Roslyn\csc.exe",
-                Environment.GetEnvironmentVariable("CSC") ?? ""
-            };
-
-            string cscPath = cscPaths.FirstOrDefault(File.Exists);
-            if (cscPath == null)
-            {
-                throw new CompilerTestException(TestCaseError.GeneratedSourceCompileFailed, "Could not find MSBuild");
-            }
-
-            return cscPath;
         }
     }
 }

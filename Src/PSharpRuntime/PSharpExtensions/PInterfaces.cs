ec4ee77398d7e36dab3872c66f665cbe16d264dc
Ankush Desai <ankushdesai@gmail.com>
2018-09-20 01:13:23 -0700

    Added missing files

---
+++ Src/PSharpRuntime/PSharpExtensions/PInterfaces.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PSharpExtensions
+{
+    public class PInterfaces
+    {
+        private static readonly Dictionary<string, List<string>> Interfaces = new Dictionary<string, List<string>>();
+
+        public static void AddInterface(string interfaceName, params string[] permissions)
+        {
+            Interfaces.Add(interfaceName, permissions.ToList());
+        }
+
+        public static List<string> GetPermissions(string interfaceName)
+        {
+            return Interfaces[interfaceName].ToList();
+        }
+    }
+}

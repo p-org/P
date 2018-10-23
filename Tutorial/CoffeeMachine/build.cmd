4f76b3eefeadad7961530ede533101b54f1a4e99
Ankush Desai <ankushdesai@gmail.com>
2018-10-22 21:55:29 -0700

    some fixes to the samples in tutorial folder

--- Tutorial/CoffeeMachine/build.cmd
+++ Tutorial/CoffeeMachine/build.cmd
@@ -1,25 +1,17 @@
 set THISDIR=%~dp0
 pushd %THISDIR%
-set pc=..\..\bld\drops\Release\x64\Binaries\pc.exe
+set pc=..\..\bld\drops\Release\Binaries\win-x64\pc.exe
 if not exist "%pc%" goto :noP
 
-set pt=..\..\bld\drops\Release\x64\Binaries\pt.exe
-
-%pc% /generate:C# /shared CoffeeMachine.p CoffeeMachineController.p Timer.p User.p Main.p Safety.p /t:CoffeeMachine.4ml
+%pc% -generate:P# -t:CoffeeMachine Timer.p User.p Main.p CoffeeMachine.p Safety.p CoffeeMachineController.p TestScript.p
 
 if NOT errorlevel 0 goto :eof
 
-%pc% /generate:C# /link /shared TestScript.p /r:CoffeeMachine.4ml
+%pc% -generate:C -t:CoffeeMachine Timer.p User.p Main.p CoffeeMachine.p Safety.p CoffeeMachineController.p TestScript.p
 
 if NOT errorlevel 0 goto :eof
-
-%pt% /psharp Test0.dll
-
-%pt% /psharp Test1.dll
-
-%pt% /psharp Test2.dll
 
 goto :eof
 :noP
-echo please run ..\..\bld\build release x64
+echo please build P
 exit /b 1
--- Tutorial/PingPong/TestScript.p
+++ Tutorial/PingPong/TestScript.p
@@ -3,6 +3,6 @@ module System =
     ClientMachine, ServerMachine, Timer
 };
 
-test Test0: main Test_1_Machine in (assert Safety in (union { Test_1_Machine }, System));
+test Test0[main = Test_1_Machine]: (assert Safety in (compose { Test_1_Machine }, System));
 
-test Test1: main Test_2_Machine in (assert Liveness in (union { Test_2_Machine }, System));
+test Test1[main = Test_2_Machine]: (assert Liveness in (compose { Test_2_Machine }, System));

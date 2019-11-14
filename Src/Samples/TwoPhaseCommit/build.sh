31e108fb77fe7cdc23faf6d6b30006858b8786aa
Ankush Desai <ankushdesai@users.noreply.github.com>
2019-11-13 20:56:53 -0800

    Delete build.sh

--- Src/Samples/TwoPhaseCommit/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR="$(dirname "${0}")"
-PC_PATH="${SCRIPT_DIR}/../../../Bld/Drops/Release/Binaries/Pc.dll"
-
-dotnet "${PC_PATH}" ./PSrc/Client.p ./PSrc/Coordinator.p ./PSrc/Participant.p ./PSrc/Events.p ./PSrc/Spec.p ./PSrc/TestDriver.p ./PSrc/Timer.p -generate:P# -t:Main
-
-dotnet publish -f netcoreapp2.1 ./TwoPhaseCommit.csproj

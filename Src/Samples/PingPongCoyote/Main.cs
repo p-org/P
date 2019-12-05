c182f833c0284a017c8e5a7c568217e82893e58a
Pantazis Deligiannis <pdeligia@microsoft.com>
2019-12-04 19:37:26 -0800

    fix

--- Src/Samples/PingPongCoyote/Main.cs
+++ Src/Samples/PingPongCoyote/Main.cs
@@ -91,48 +91,28 @@ namespace pingpong
 
         public Transition Anon()
         {
-            try
-            {
-                Main currentMachine = this;
+            Main currentMachine = this;
                 PMachineValue TMP_tmp0 = null;
                 PEvent TMP_tmp1 = null;
                 TMP_tmp0 = currentMachine.CreateInterface<I_PONG>(currentMachine);
                 pongId = TMP_tmp0;
                 TMP_tmp1 = new Success(null);
-                currentMachine.TryRaiseEvent(TMP_tmp1);
-                throw new PUnreachableCodeException();
-            }
-            catch (PMachineTransitionException ex)
-            {
-                return ex.Transition;
-            }
-
-            return default;
+                return currentMachine.TryRaiseEvent(TMP_tmp1);
         }
 
         public Transition Anon_1()
         {
-            try
-            {
-                Main currentMachine = this;
-                PMachineValue TMP_tmp0_1 = null;
-                PEvent TMP_tmp1_1 = null;
-                PMachineValue TMP_tmp2 = null;
-                PEvent TMP_tmp3 = null;
-                TMP_tmp0_1 = ((PMachineValue)((IPrtValue)pongId)?.Clone());
-                TMP_tmp1_1 = new Ping(null);
-                TMP_tmp2 = currentMachine.self;
-                currentMachine.TrySendEvent(TMP_tmp0_1, TMP_tmp1_1, TMP_tmp2);
-                TMP_tmp3 = new Success(null);
-                currentMachine.TryRaiseEvent(TMP_tmp3);
-                throw new PUnreachableCodeException();
-            }
-            catch (PMachineTransitionException ex)
-            {
-                return ex.Transition;
-            }
-
-            return default;
+            Main currentMachine = this;
+            PMachineValue TMP_tmp0_1 = null;
+            PEvent TMP_tmp1_1 = null;
+            PMachineValue TMP_tmp2 = null;
+            PEvent TMP_tmp3 = null;
+            TMP_tmp0_1 = ((PMachineValue)((IPrtValue)pongId)?.Clone());
+            TMP_tmp1_1 = new Ping(null);
+            TMP_tmp2 = currentMachine.self;
+            currentMachine.TrySendEvent(TMP_tmp0_1, TMP_tmp1_1, TMP_tmp2);
+            TMP_tmp3 = new Success(null);
+            return currentMachine.TryRaiseEvent(TMP_tmp3);
         }
 
         [Start]
@@ -194,27 +174,17 @@ namespace pingpong
 
         public Transition Anon_3(Event currentMachine_dequeuedEvent)
         {
-            try
-            {
-                PONG currentMachine = this;
-                PMachineValue payload = (PMachineValue)(gotoPayload ?? ((PEvent)currentMachine_dequeuedEvent).Payload);
-                gotoPayload = null;
-                PMachineValue TMP_tmp0_2 = null;
-                PEvent TMP_tmp1_2 = null;
-                PEvent TMP_tmp2_1 = null;
-                TMP_tmp0_2 = ((PMachineValue)((IPrtValue)payload)?.Clone());
-                TMP_tmp1_2 = new Pong(null);
-                currentMachine.TrySendEvent(TMP_tmp0_2, TMP_tmp1_2);
-                TMP_tmp2_1 = new Success(null);
-                currentMachine.TryRaiseEvent(TMP_tmp2_1);
-                throw new PUnreachableCodeException();
-            }
-            catch (PMachineTransitionException ex)
-            {
-                return ex.Transition;
-            }
-
-            return default;
+            PONG currentMachine = this;
+            PMachineValue payload = (PMachineValue)(gotoPayload ?? ((PEvent)currentMachine_dequeuedEvent).Payload);
+            gotoPayload = null;
+            PMachineValue TMP_tmp0_2 = null;
+            PEvent TMP_tmp1_2 = null;
+            PEvent TMP_tmp2_1 = null;
+            TMP_tmp0_2 = ((PMachineValue)((IPrtValue)payload)?.Clone());
+            TMP_tmp1_2 = new Pong(null);
+            currentMachine.TrySendEvent(TMP_tmp0_2, TMP_tmp1_2);
+            TMP_tmp2_1 = new Success(null);
+            return currentMachine.TryRaiseEvent(TMP_tmp2_1);
         }
 
         [Start]

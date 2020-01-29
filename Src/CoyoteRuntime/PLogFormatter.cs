0bc1825837ee05bb6c0bd150eb526fbe6759c079
Chris Lovett <clovett@microsoft.com>
2020-01-28 16:23:55 -0800

    update to rc9 with Event e in the IActorRuntimeLog.

--- Src/CoyoteRuntime/PLogFormatter.cs
+++ Src/CoyoteRuntime/PLogFormatter.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Coyote.Actors;
+﻿using Microsoft.Coyote;
+using Microsoft.Coyote.Actors;
 using Microsoft.Coyote.Runtime;
 using Microsoft.Coyote.Runtime.Logging;
 using Plang.PrtSharp.Exceptions;
@@ -23,37 +24,37 @@ namespace Plang.PrtSharp
                 return;
             }
 
-            base.OnStateTransition(id, stateName.Split('.').Last(), isEntry);
+            base.OnStateTransition(id, stateName, isEntry);
         }
 
         public override void OnDefaultEventHandler(ActorId id, string stateName)
         {
-            base.OnDefaultEventHandler(id, stateName.Split('.').Last());
+            base.OnDefaultEventHandler(id, stateName);
         }
 
         public override void OnPopState(ActorId id, string currStateName, string restoredStateName)
         {
-            base.OnPopState(id, currStateName.Split('.').Last(), restoredStateName.Split('.').Last());
+            base.OnPopState(id, currStateName, restoredStateName);
         }
 
-        public override void OnPopUnhandledEvent(ActorId id, string stateName, string eventName)
+        public override void OnPopUnhandledEvent(ActorId id, string stateName, Event e)
         {
-            base.OnPopUnhandledEvent(id, stateName.Split('.').Last(), eventName);
+            base.OnPopUnhandledEvent(id, stateName, e);
         }
 
         public override void OnPushState(ActorId id, string currStateName, string newStateName)
         {
-            base.OnPushState(id, currStateName.Split('.').Last(), newStateName.Split('.').Last());
+            base.OnPushState(id, currStateName, newStateName);
         }
 
         public override void OnWaitEvent(ActorId id, string stateName, params Type[] eventTypes)
         {
-            base.OnWaitEvent(id, stateName.Split('.').Last(), eventTypes);
+            base.OnWaitEvent(id, stateName, eventTypes);
         }
 
         public override void OnWaitEvent(ActorId id, string stateName, Type eventType)
         {
-            base.OnWaitEvent(id, stateName.Split('.').Last(), eventType);
+            base.OnWaitEvent(id, stateName, eventType);
         }
 
         public override void OnMonitorStateTransition(string monitorTypeName, ActorId id, string stateName, bool isEntry, bool? isInHotState)
@@ -63,7 +64,7 @@ namespace Plang.PrtSharp
                 return;
             }
 
-            base.OnMonitorStateTransition(monitorTypeName, id, stateName.Split('.').Last(), isEntry, isInHotState);
+            base.OnMonitorStateTransition(monitorTypeName, id, stateName, isEntry, isInHotState);
         }
 
         public override void OnCreateActor(ActorId id, ActorId creator)
@@ -76,44 +77,46 @@ namespace Plang.PrtSharp
             base.OnCreateActor(id, creator);
         }
 
-        public override void OnDequeueEvent(ActorId id, string stateName, string eventName)
+        public override void OnDequeueEvent(ActorId id, string stateName, Event e)
         {
+
             if (stateName.Contains("__InitState__") || id.Name.Contains("GodMachine"))
             {
                 return;
             }
 
-            base.OnDequeueEvent(id, stateName.Split('.').Last(), eventName.Split('.').Last());
+            base.OnDequeueEvent(id, stateName, e);
         }
 
-        public override void OnRaiseEvent(ActorId id, string stateName, string eventName)
+        public override void OnRaiseEvent(ActorId id, string stateName, Event e)
         {
+            string eventName = e.GetType().FullName;
             if (stateName.Contains("__InitState__") || id.Name.Contains("GodMachine") || eventName.Contains("GotoStateEvent"))
             {
                 return;
             }
 
-            base.OnRaiseEvent(id, stateName.Split('.').Last(), eventName.Split('.').Last());
+            base.OnRaiseEvent(id, stateName, e);
         }
 
-        public override void OnEnqueueEvent(ActorId id, string eventName)
+        public override void OnEnqueueEvent(ActorId id, Event e)
         {
-            base.OnEnqueueEvent(id, eventName.Split('.').Last());
+            base.OnEnqueueEvent(id, e);
         }
 
-        public override void OnReceiveEvent(ActorId id, string stateName, string eventName, bool wasBlocked)
+        public override void OnReceiveEvent(ActorId id, string stateName, Event e, bool wasBlocked)
         {
-            base.OnReceiveEvent(id, stateName.Split('.').Last(), eventName.Split('.').Last(), wasBlocked);
+            base.OnReceiveEvent(id, stateName, e, wasBlocked);
         }
 
-        public override void OnMonitorRaiseEvent(string monitorTypeName, ActorId id, string stateName, string eventName)
+        public override void OnMonitorRaiseEvent(string monitorTypeName, ActorId id, string stateName, Event e)
         {
-            base.OnMonitorRaiseEvent(monitorTypeName, id, stateName.Split('.').Last(), eventName.Split('.').Last());
+            base.OnMonitorRaiseEvent(monitorTypeName, id, stateName, e);
         }
 
-        public override void OnSendEvent(ActorId targetActorId, ActorId senderId, string senderStateName, string eventName, Guid opGroupId, bool isTargetHalted)
+        public override void OnSendEvent(ActorId targetActorId, ActorId senderId, string senderStateName, Event e, Guid opGroupId, bool isTargetHalted)
         {
-            base.OnSendEvent(targetActorId, senderId, senderStateName.Split('.').Last(), eventName.Split('.').Last(), opGroupId, isTargetHalted);
+            base.OnSendEvent(targetActorId, senderId, senderStateName, e, opGroupId, isTargetHalted);
         }
 
         public override void OnGotoState(ActorId id, string currStateName, string newStateName)
@@ -123,7 +126,7 @@ namespace Plang.PrtSharp
                 return;
             }
 
-            base.OnGotoState(id, currStateName.Split('.').Last(), newStateName.Split('.').Last());
+            base.OnGotoState(id, currStateName, newStateName);
         }
 
         public override void OnExecuteAction(ActorId id, string stateName, string actionName)
@@ -136,12 +139,12 @@ namespace Plang.PrtSharp
 
         public override void OnExceptionHandled(ActorId id, string stateName, string actionName, Exception ex)
         {
-            base.OnExceptionHandled(id, stateName.Split('.').Last(), actionName, ex);
+            base.OnExceptionHandled(id, stateName, actionName, ex);
         }
 
         public override void OnExceptionThrown(ActorId id, string stateName, string actionName, Exception ex)
         {
-            base.OnExceptionThrown(id, stateName.Split('.').Last(), actionName, ex);
+            base.OnExceptionThrown(id, stateName, actionName, ex);
         }
     }
 }

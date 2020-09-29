687b0d596e8e4eca12d34944fd4bb9b19e944b8d
Ankush Desai <ankushpd@amazon.com>
2020-09-29 15:36:40 -0700

    Cleaned up the scripts

--- Bld/build.ps1
+++ Bld/build.ps1
@@ -1,11 +1,13 @@
-pushd (Join-Path $PSScriptRoot ..)
+pushd ..
 
 # Initialize submodules
+Write-Host "Initializing submodules .."  -ForegroundColor DarkGreen -BackgroundColor White
 git submodule update --init --recursive
 
 # Run the build! :D
-dotnet publish -c Release .\Src\PCompiler\CommandLine\CommandLine.csproj
-
-echo "Compiler located in Bld\Drops\Release\Binaries\Pc.exe"
-
+dotnet build -c Release
+$x = Get-Location
+Write-Host "----------------------------------------------"  -ForegroundColor DarkRed -BackgroundColor White
+Write-Host "P Compiler is located at $x\Bld\Drops\Release\Binaries\Pc.dll"  -ForegroundColor DarkGreen -BackgroundColor White
+Write-Host "----------------------------------------------"  -ForegroundColor DarkRed -BackgroundColor White
 popd
--- Bld/build.sh
+++ Bld/build.sh
@@ -6,12 +6,13 @@ ORANGE='\033[0;33m'
 set -e
 
 echo -e "${ORANGE} ---- Fetching git submodules ----${NOCOLOR}"
+pushd ..
 # Initialize submodules
 git submodule update --init --recursive
 
 echo -e "${ORANGE} ---- Building the PCompiler ----${NOCOLOR}"
 # Run the build!
-pushd ..
+
 dotnet build -c Release
 echo -e "${GREEN} ----------------------------------${NOCOLOR}"
 echo -e "${GREEN} P Compiler located in ${PWD}/Bld/Drops/Release/Binaries/Pc.dll${NOCOLOR}"

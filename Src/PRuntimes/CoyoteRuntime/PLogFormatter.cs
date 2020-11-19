640969b84fd898e100a5487c2182ea0081978636
Ankush Desai <ankushpd@amazon.com>
2020-11-18 21:40:52 -0800

    Removed logging of EnqueueLog

--- Src/PRuntimes/CoyoteRuntime/PLogFormatter.cs
+++ Src/PRuntimes/CoyoteRuntime/PLogFormatter.cs
@@ -144,14 +144,7 @@ namespace Plang.CoyoteRuntime
             this.Logger.WriteLine(text);
         }
 
-        public override void OnEnqueueEvent(ActorId id, Event e)
-        {
-            
-            string eventName = this.GetEventNameWithPayload(e);
-            string text = $"<EnqueueLog> '{id}' enqueued event '{eventName}'.";
-            this.Logger.WriteLine(text);
-            
-        }
+        public override void OnEnqueueEvent(ActorId id, Event e) {   }
 
         public override void OnReceiveEvent(ActorId id, string stateName, Event e, bool wasBlocked)
         {

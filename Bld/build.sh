559ca9149bc62c2f08cf7eb40e1db00614b59f68
Ankush Desai <ankushpd@amazon.com>
2020-12-08 14:23:11 -0800

    Fixing the path in the the build script

--- Bld/build.sh
+++ Bld/build.sh
@@ -23,13 +23,13 @@ mkdir -p Bld/Drops/RVM
 cp Src/PRuntimes/RvmRuntime/target/*.jar Bld/Drops/RVM
 
 echo -e "${GREEN} ----------------------------------${NOCOLOR}"
-echo -e "${GREEN} P Compiler located in ${PWD}/Bld/Drops/Release/Binaries/Pc.dll${NOCOLOR}"
+echo -e "${GREEN} P Compiler located in ${PWD}/Bld/Drops/Release/Binaries/netcoreapp3.1/P.dll${NOCOLOR}"
 echo -e "${GREEN} ----------------------------------${NOCOLOR}"
 
 
 echo -e "${GREEN} Shortcuts:: (add the following lines (aliases) to your bash_profile) ${NOCOLOR}"
 echo -e "${GREEN} ----------------------------------${NOCOLOR}"
-echo -e "${ORANGE} alias pc='dotnet ${PWD}/Bld/Drops/Release/Binaries/Pc.dll'${NOCOLOR}"
+echo -e "${ORANGE} alias pc='dotnet ${PWD}/Bld/Drops/Release/Binaries/netcoreapp3.1/P.dll'${NOCOLOR}"
 echo -e "${ORANGE} alias pmc='dotnet ${PWD}/packages/microsoft.coyote/1.0.5/lib/netcoreapp3.1/coyote.dll test'${NOCOLOR}"
 echo -e "${GREEN} ----------------------------------${NOCOLOR}"
 popd

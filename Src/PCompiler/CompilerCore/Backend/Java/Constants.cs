70b80afde1b4ed4bcd529dcf702d60ff661e711e
Nathan Taylor <nathta@amazon.com>
2022-06-30 13:57:58 -0700

    [Java] Generate fat JARs with the prt dependency

--- Src/PCompiler/CompilerCore/Backend/Java/Constants.cs
+++ Src/PCompiler/CompilerCore/Backend/Java/Constants.cs
@@ -91,10 +91,38 @@ xsi:schemaLocation=""http://maven.apache.org/POM/4.0.0 http://maven.apache.org/x
             <groupId>p.runtime</groupId>
             <artifactId>PJavaRuntime</artifactId>
             <version>1.0-SNAPSHOT</version>
+
+            <!-- Do not transitively bundle log4j as whoever uses this jar will also depend on it. -->
+            <exclusions>
+                <exclusion>
+                    <groupId>org.apache.logging.log4j</groupId>
+                    <artifactId>*</artifactId>
+                </exclusion>
+            </exclusions>
         </dependency>
     </dependencies>
 
     <build>
+        <plugins>
+            <plugin>
+                <artifactId>maven-assembly-plugin</artifactId>
+                <version>3.3.0</version>
+                <configuration>
+                    <descriptorRefs>
+                        <descriptorRef>jar-with-dependencies</descriptorRef>
+                    </descriptorRefs>
+                </configuration>
+                <executions>
+                    <execution>
+                        <id>make-assembly</id> <!-- this is used for inheritance merges -->
+                        <phase>package</phase> <!-- bind to the packaging phase -->
+                        <goals>
+                            <goal>single</goal>
+                        </goals>
+                    </execution>
+                </executions>
+            </plugin>
+        </plugins>
         <directory>${{buildDirectory}}</directory>
         <sourceDirectory>.</sourceDirectory>
     </build>

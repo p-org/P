ee58a1db7b53809464923757407c7f45c88c0602
Aman Goel <goelaman@amazon.com>
2022-11-02 11:00:51 -0700

    [PSym] Minor change

--- Src/PCompiler/CompilerCore/Backend/Symbolic/Constants.cs
+++ Src/PCompiler/CompilerCore/Backend/Symbolic/Constants.cs
@@ -5,15 +5,17 @@ namespace Plang.Compiler.Backend.Symbolic
     internal class Constants
     {
         internal static readonly string pomTemplate =
-            @"
-<?xml version=""1.0"" encoding=""UTF-8""?>
+            @"<?xml version=""1.0"" encoding=""UTF-8""?>
 <project xmlns=""http://maven.apache.org/POM/4.0.0""
 xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance""
 xsi:schemaLocation=""http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd"">
+
     <modelVersion>4.0.0</modelVersion>
+
     <groupId>psymbolic</groupId>
     <artifactId>projectName</artifactId>
     <version>1.0</version>
+
     <build>
         <sourceDirectory>.</sourceDirectory>
         <plugins>
@@ -45,6 +47,7 @@ xsi:schemaLocation=""http://maven.apache.org/POM/4.0.0 http://maven.apache.org/x
             </plugin>
         </plugins>
     </build>
+
     <dependencies>
         <dependency>
             <groupId>io.github.p-org</groupId>
@@ -54,18 +57,20 @@ xsi:schemaLocation=""http://maven.apache.org/POM/4.0.0 http://maven.apache.org/x
 
         <!-- https://mvnrepository.com/artifact/org.projectlombok/lombok -->
         <dependency>
-        <groupId>org.projectlombok</groupId>
-        <artifactId>lombok</artifactId>
-        <version>1.18.24</version>
-        <scope>provided</scope>
+            <groupId>org.projectlombok</groupId>
+            <artifactId>lombok</artifactId>
+            <version>1.18.24</version>
+            <scope>provided</scope>
         </dependency>
     </dependencies>
+
     <properties>
         <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
         <maven.compiler.source>1.8</maven.compiler.source>
         <maven.compiler.target>1.8</maven.compiler.target>
         <java.version>16</java.version>
     </properties>
-</project>";
+</project>
+";
     }
 }

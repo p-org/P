1537aa00d67c7f90c534745dfff6b68a16e51026
Aman Goel <goelaman@amazon.com>
2023-04-24 15:56:33 -0700

    Update compiled jar auto-detect for p check command

--- Src/PCompiler/PCommandLine/Options/PCheckerOptions.cs
+++ Src/PCompiler/PCommandLine/Options/PCheckerOptions.cs
@@ -307,6 +307,16 @@ namespace Plang.Options
                             || fileName.EndsWith("/p.dll"))
                             continue;
                     }
+                    else if (checkerConfiguration.Mode == CheckerMode.Verification || checkerConfiguration.Mode == CheckerMode.Coverage)
+                    {
+                        if (!fileName.Contains("Symbolic/"))
+                            continue;
+                    }
+                    else
+                    {
+                        if (!fileName.Contains("java/"))
+                            continue;
+                    }
                     checkerConfiguration.AssemblyToBeAnalyzed = fileName;
                     CommandLineOutput.WriteInfo($".. Found a P compiled file: {checkerConfiguration.AssemblyToBeAnalyzed}");
                     break;

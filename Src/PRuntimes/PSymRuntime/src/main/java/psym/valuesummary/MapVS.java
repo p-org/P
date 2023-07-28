50027d031f474d06e5dd5260bb7c9c166576311b
Aman Goel <goelaman@amazon.com>
2023-07-27 18:56:21 -0700

    [PSym] minor

--- Src/PRuntimes/PSymRuntime/src/main/java/psym/valuesummary/MapVS.java
+++ Src/PRuntimes/PSymRuntime/src/main/java/psym/valuesummary/MapVS.java
@@ -231,8 +231,8 @@ public class MapVS<K, T extends ValueSummary<T>, V extends ValueSummary<V>>
     }
 
     MapVS<K, T, V> result = new MapVS<>(newKeys, newEntries);
-    assert result.containsKey(keySummary).getGuardFor(true).equals(keySummary.getUniverse());
-    assert (result.get(keySummary).symbolicEquals(valSummary, Guard.constTrue()).getGuardFor(true).equals(valSummary.getUniverse()));
+//    assert result.containsKey(keySummary).getGuardFor(true).equals(keySummary.getUniverse());
+//    assert (result.get(keySummary).symbolicEquals(valSummary, Guard.constTrue()).getGuardFor(true).equals(valSummary.getUniverse()));
     return result;
   }
 

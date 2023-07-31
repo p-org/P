fbf9c343f76b372a0f4633edbf705d5d27724012
Ao Li <aolm@amazon.com>
2023-07-31 16:22:56 -0700

    Fix mutation size and mutation count.

--- Src/PChecker/CheckerCore/Generator/Mutator/Utils.cs
+++ Src/PChecker/CheckerCore/Generator/Mutator/Utils.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using PChecker.Generator.Object;
 
 namespace PChecker.Generator.Mutator;
@@ -12,6 +13,8 @@ public class Utils
     public static RandomChoices<T> MutateRandomChoices<T> (RandomChoices<T> randomChoices, int meanMutationCount, int meanMutationSize, System.Random random)
         where T: IConvertible
     {
+        meanMutationCount = Math.Max(Math.Min(randomChoices.Data.Count / 5, meanMutationCount), 1);
+        meanMutationSize = Math.Max(Math.Min(randomChoices.Data.Count / 5, meanMutationSize), 1);
         RandomChoices<T> newChoices = new RandomChoices<T>(randomChoices);
         int mutations = Utils.SampleGeometric(1.0f / meanMutationCount, random.NextDouble());
 

96158b94a6a24ca647341954450ba5d8d2c1f8a4
Ao Li <aolm@amazon.com>
2023-08-02 13:56:18 -0700

    Add state name to the `EventObj`.

--- Src/PChecker/CheckerCore/Pattern/EventObj.cs
+++ Src/PChecker/CheckerCore/Pattern/EventObj.cs
@@ -1,4 +1,5 @@
 using PChecker.Actors.Events;
+using PChecker.Specifications.Monitors;
 
 namespace PChecker.Matcher;
 
@@ -7,13 +8,15 @@ public class EventObj
     public Event Event;
     public string? Sender;
     public string? Receiver;
+    public string State;
     public int Index;
 
-    public EventObj(Event e, string? sender, string? receiver, int index)
+    public EventObj(Event e, string? sender, string? receiver, string state, int index)
     {
         Event = e;
         Sender = sender;
         Receiver = receiver;
+        State = state;
         Index = index;
     }
 }

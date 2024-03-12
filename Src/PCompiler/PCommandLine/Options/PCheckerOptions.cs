3e34ea067c8e330426080d90d8c273d452153b19
Aman Goel <goelaman@amazon.com>
2024-03-12 17:25:58 +0000

    [CLI] Adds new pcover cli (hidden)

--- Src/PCompiler/PCommandLine/Options/PCheckerOptions.cs
+++ Src/PCompiler/PCommandLine/Options/PCheckerOptions.cs
@@ -31,7 +31,7 @@ namespace Plang.Options
             basicOptions.AddPositionalArgument("path", "Path to the compiled file to check for correctness (*.dll)."+
                                                        " If this option is not passed, the compiler searches for a *.dll file in the current folder").IsRequired = false;
             var modes = basicOptions.AddArgument("mode", "md", "Choose a checker mode (options: bugfinding, verification, coverage, pobserve). (default: bugfinding)");
-            modes.AllowedValues = new List<string>() { "bugfinding", "verification", "coverage", "pobserve" };
+            modes.AllowedValues = new List<string>() { "bugfinding", "verification", "coverage", "pobserve", "coverage_new" };
             modes.IsHidden = true;
             basicOptions.AddArgument("testcase", "tc", "Test case to explore");
             basicOptions.AddArgument("smoke-testing", "tsmoke",

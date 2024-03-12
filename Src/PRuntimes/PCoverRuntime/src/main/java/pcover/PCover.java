efc656a2eabaf8c5e74cf948bc235b40314fe37d
Aman Goel <goelaman@amazon.com>
2024-03-12 18:14:34 +0000

    [PCover runtime] Adds basic runtime setup files

---
+++ Src/PRuntimes/PCoverRuntime/src/main/java/pcover/PCover.java
@@ -0,0 +1,24 @@
+package pcover;
+
+import org.reflections.Reflections;
+import pcover.runtime.logger.Log4JConfig;
+import pcover.utils.exceptions.NotImplementedException;
+
+/**
+ * PCover runtime top-level class
+ */
+public class PCover {
+
+  /**
+   * Main entry point for PCover runtime.
+   */
+  public static void main(String[] args) {
+    // configure Log4J
+    Log4JConfig.configureLog4J();
+
+    // get reflection to fetch pcover model IR
+    Reflections reflections = new Reflections("pcover.model");
+
+    throw new NotImplementedException();
+  }
+}

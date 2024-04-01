0d43c6a6c44d40707204ccb5937903120df3da40
Aman Goel <goelaman@amazon.com>
2024-04-01 21:51:54 +0000

    [Pexplicit RT] Adds utils for timed call, random number generator, and in-line asserts

---
+++ Src/PRuntimes/PExplicitRuntime/src/main/java/pexplicit/utils/monitor/TimedCall.java
@@ -0,0 +1,29 @@
+package pexplicit.utils.monitor;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeoutException;
+
+import pexplicit.runtime.scheduler.Scheduler;
+import pexplicit.utils.exceptions.BugFoundException;
+import pexplicit.utils.exceptions.MemoutException;
+
+public class TimedCall implements Callable<Integer> {
+  private final Scheduler scheduler;
+
+  public TimedCall(Scheduler scheduler, boolean resume) {
+    this.scheduler = scheduler;
+  }
+
+  @Override
+  public Integer call()
+      throws MemoutException, BugFoundException, TimeoutException, InterruptedException {
+    try {
+      this.scheduler.doSearch();
+    } catch (OutOfMemoryError e) {
+      throw new MemoutException(e.getMessage(), MemoryMonitor.getMemSpent(), e);
+    } catch (MemoutException | BugFoundException | TimeoutException | InterruptedException e) {
+      throw e;
+    }
+      return 0;
+  }
+}

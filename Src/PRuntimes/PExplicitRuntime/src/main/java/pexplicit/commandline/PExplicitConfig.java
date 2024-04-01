901fcb4d49dddb1732ad83f85a41e2da279b5178
Aman Goel <goelaman@amazon.com>
2024-04-01 21:48:28 +0000

    [PExplicit RT] Adds CLI options and config

--- Src/PRuntimes/PExplicitRuntime/src/main/java/pexplicit/commandline/PExplicitConfig.java
+++ Src/PRuntimes/PExplicitRuntime/src/main/java/pexplicit/commandline/PExplicitConfig.java
@@ -1,7 +1,50 @@
 package pexplicit.commandline;
 
+import lombok.Getter;
+import lombok.Setter;
+import pexplicit.runtime.machine.buffer.BufferSemantics;
+
 /**
  * Represents the configuration for PExplicit runtime.
  */
+@Getter
 public class PExplicitConfig {
+    // default name of the test driver
+    final String testDriverDefault = "DefaultImpl";
+    // max internal steps before throwing an exception
+    final int maxInternalSteps = 100;
+    // name of the test driver
+    @Setter
+    String testDriver = testDriverDefault;
+    // name of the project
+    @Setter String projectName = "default";
+    // name of the output folder
+    @Setter String outputFolder = "output";
+    // time limit in seconds (0 means infinite)
+    @Setter double timeLimit = 0;
+    // memory limit in megabytes (0 means infinite)
+    @Setter double memLimit = (Runtime.getRuntime().maxMemory() / 2.0 / 1024.0 / 1024.0);
+    // level of verbosity for the logging
+    @Setter int verbosity = 0;
+    // strategy of exploration
+    @Setter String strategy = "dfs";
+    // max number of schedules bound provided by the user
+    @Setter int maxSchedules = 1;
+    // max steps/depth bound provided by the user
+    @Setter int maxStepBound = 10000;
+    // fail on reaching the maximum scheduling step bound
+    @Setter boolean failOnMaxStepBound = false;
+    // random seed
+    @Setter long randomSeed = System.currentTimeMillis();
+    // buffer semantics
+    @Setter
+    BufferSemantics bufferSemantics = BufferSemantics.SenderQueue;
+
+    public void setToDfs() {
+        this.setStrategy("dfs");
+    }
+
+    public void setToReplay() {
+        this.setStrategy("replay");
+    }
 }

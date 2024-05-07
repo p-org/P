4bee0665c376d62a786822c40878c54b6bbaf2f7
Aman Goel <goelaman@amazon.com>
2024-05-07 23:03:08 +0000

    [PEx] Support Java foreign functions

---
+++ Src/PRuntimes/PExplicitRuntime/src/main/java/pexplicit/runtime/ForeignFunctionInterface.java
@@ -0,0 +1,28 @@
+package pexplicit.runtime;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class ForeignFunctionInterface {
+  /**
+   * Invoke a foreign function with a void return type
+   *
+   * @param fn function to invoke
+   * @param args arguments
+   */
+  public static void accept(Consumer<List<Object>> fn, Object... args) {
+    fn.accept(List.of(args));
+  }
+
+  /**
+   * Invoke a foreign function with a non-void return type
+   *
+   * @param fn function to invoke
+   * @param args arguments
+   * @return the return value of the function
+   */
+  public static Object apply(Function<List<Object>, Object> fn, Object... args) {
+    return fn.apply(List.of(args));
+  }
+}

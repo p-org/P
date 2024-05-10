e0dfaa4e71b9342a778d86409f75f3d7907ac98b
Ao Li <leeleo3x@gmail.com>
2024-05-10 16:07:46 -0400

    update.

--- Src/PChecker/CheckerCore/CheckerConfiguration.cs
+++ Src/PChecker/CheckerCore/CheckerConfiguration.cs
@@ -229,7 +229,7 @@ namespace PChecker
         /// Defaults to true.
         /// </summary>
         [DataMember]
-        public bool IsJsonLogEnabled { get; set; } = true;
+        public bool IsJsonLogEnabled { get; set; } = false;
 
         /// <summary>
         /// If specified, requests a custom runtime log to be used instead of the default.

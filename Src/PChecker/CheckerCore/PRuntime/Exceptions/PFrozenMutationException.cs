78d90889ee72ca430d0a9c95844223387e078ce3
Ankush Desai <ankushpd@amazon.com>
2024-05-21 16:16:50 -0700

    Moved the CSharpRuntime for P into the CheckerCore

---
+++ Src/PChecker/CheckerCore/PRuntime/Exceptions/PFrozenMutationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PChecker.PRuntime.Exceptions
+{
+    public class PFrozenMutationException : Exception
+    {
+        public PFrozenMutationException()
+        {
+        }
+
+        public PFrozenMutationException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ Src/PChecker/CheckerCore/PRuntime/Exceptions/PUnReachableCodeException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PChecker.PRuntime.Exceptions
+{
+    public class PUnreachableCodeException : Exception
+    {
+        public PUnreachableCodeException()
+        {
+        }
+
+        public PUnreachableCodeException(string message) : base(message)
+        {
+        }
+
+        public PUnreachableCodeException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}

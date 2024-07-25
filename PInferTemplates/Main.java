d8562b3dc347732cdbcf81ede07c356a7c76864c
AD1024 <mikedh@amazon.com>
2024-07-25 18:14:58 +0000

    [fix] task pool monitors numFinished instead of ptr

--- PInferTemplates/Main.java
+++ PInferTemplates/Main.java
@@ -142,6 +142,7 @@ public class Main {
         }
         System.out.println("Forall/Exists-only Number of tasks: " + numTasks);
         taskPool.waitForAll();
+        System.out.println("Number properties mined: " + taskPool.getNumMined());
     }
 
     private static void specMiningForallExists(List<RawPredicate> predicateList,
@@ -216,6 +217,7 @@ public class Main {
         }
         System.out.println("Forall-Exists Number of tasks: " + numTasks);
         taskPool.waitForAll();
+        System.out.println("Number properties mined: " + taskPool.getNumMined());
     }
 
     public static void main(String[] args) throws Exception {

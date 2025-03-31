c7e86ab370700b21d32f23053aecd7c2ce0f3eb2
Zhe Zhou <zorichzorich@gmail.com>
2025-03-31 10:04:05 -0700

    T-wise combinatorial test (#837)
    * add twise
    
    * clean the code

--- Src/PCompiler/CompilerCore/TypeChecker/ParamAssignment.cs
+++ Src/PCompiler/CompilerCore/TypeChecker/ParamAssignment.cs
@@ -8,32 +8,107 @@ namespace Plang.Compiler.TypeChecker;
 
 public abstract class ParamAssignment
 {
+    private static IEnumerable<IEnumerable<T>> DifferentCombinations<T>(IEnumerable<T> elements, int k)
+    {
+        var enumerable = elements as T[] ?? elements.ToArray();
+        return k == 0
+            ? [Array.Empty<T>()]
+            : enumerable.SelectMany((e, i) =>
+                DifferentCombinations(enumerable.Skip(i + 1), k - 1).Select(c => new[] { e }.Concat(c)));
+    }
+
+    private static Dictionary<int, T> EnumerableToIndexDict<T>(IEnumerable<T> l, Func<T, T> f)
+    {
+        return Enumerable.Range(0, l.Count()).Zip(l, (a, b) => new { a, b }).ToDictionary(x => x.a, x => f(x.b));
+    }
+
+    private static Dictionary<int, Dictionary<string, int>> MakeVectorMap(Dictionary<string, int> universe, int twise)
+    {
+        var vectorSet = new HashSet<Dictionary<string, int>>();
+        foreach (var param in DifferentCombinations(universe.Select(kv => kv.Key), twise))
+        {
+            var result = new List<Dictionary<string, int>> { new() };
+            foreach (var name in param)
+            {
+                var newResult = new List<Dictionary<string, int>>();
+                for (var i = 0; i < universe[name]; i++)
+                {
+                    var resultCopy = result.Select(dict => dict.ToDictionary(kv => kv.Key, kv => kv.Value)).ToList();
+                    foreach (var vector in resultCopy) vector.Add(name, i);
+                    newResult.AddRange(resultCopy);
+                }
+
+                result = newResult;
+            }
+
+            vectorSet.UnionWith(result);
+        }
+
+        return EnumerableToIndexDict(vectorSet, x => x);
+    }
+
+    private static Dictionary<int, HashSet<int>> MakeAssignmentCoverageMap(
+        Dictionary<int, Dictionary<string, int>> assignmentMap,
+        Dictionary<int, Dictionary<string, int>> vectorMap,
+        List<int> assignments)
+    {
+        var assignmentCoverageMap = new Dictionary<int, HashSet<int>>();
+        foreach (var assignment in assignments)
+        {
+            assignmentCoverageMap.Add(assignment, []);
+            foreach (var kv in vectorMap.Where(kv => kv.Value.All(assignmentMap[assignment].Contains)))
+                assignmentCoverageMap[assignment].Add(kv.Key);
+        }
+
+        return assignmentCoverageMap;
+    }
+
+    private static List<int> GreedyCoverageExplore(Dictionary<string, int> universe,
+        List<Dictionary<string, int>> assignmentList, int twise)
+    {
+        var assignments = Enumerable.Range(0, assignmentList.Count).ToList();
+        if (twise == universe.Count) return assignments;
+        var vectorMap = MakeVectorMap(universe, twise);
+        var assignmentMap = EnumerableToIndexDict(assignmentList, x => x);
+        var assignmentCoverageMap = MakeAssignmentCoverageMap(assignmentMap, vectorMap, assignments);
+        var obligationSet = vectorMap.Keys.ToHashSet();
+        obligationSet.IntersectWith(assignmentCoverageMap.SelectMany(kv => kv.Value));
+        foreach (var kv in assignmentCoverageMap) assignmentCoverageMap[kv.Key].IntersectWith(obligationSet);
+        var result = new List<int>();
+        while (obligationSet.Count != 0)
+        {
+            var (ass, coverage) = assignmentCoverageMap.MaxBy(kv => kv.Value.Count);
+            obligationSet.ExceptWith(coverage);
+            assignmentCoverageMap.Remove(ass);
+            foreach (var kv in assignmentCoverageMap) assignmentCoverageMap[kv.Key].ExceptWith(coverage);
+            result.Add(ass);
+        }
+
+        return result;
+    }
+
     private static Dictionary<string, IPExpr> Dic2StrDic(Dictionary<Variable, IPExpr> dic)
     {
         var dicAux = new Dictionary<string, IPExpr>();
-        foreach (var (k, i) in dic)
-        {
-            dicAux[k.Name] = i;
-        }
+        foreach (var (k, i) in dic) dicAux[k.Name] = i;
         return dicAux;
     }
 
-    private static Dictionary<Variable, IPExpr> IndexDic2Dic(List<Variable> globalParams, IDictionary<string, List<IPExpr>> paramExprDic, IDictionary<string, int> indexDic)
+    private static Dictionary<Variable, IPExpr> IndexDic2Dic(List<Variable> globalParams,
+        IDictionary<string, List<IPExpr>> paramExprDic, IDictionary<string, int> indexDic)
     {
         var dic = new Dictionary<Variable, IPExpr>();
         foreach (var (k, i) in indexDic)
         {
             var values = paramExprDic[k];
             var variable = globalParams.First(v => v.Name == k);
-            if (i >= values.Count)
-            {
-                throw new ArgumentException("Index out of range in global variable config."); 
-            }
+            if (i >= values.Count) throw new ArgumentException("Index out of range in global variable config.");
             dic[variable] = values[i];
         }
+
         return dic;
     }
-    
+
     public static string RenameSafetyTestByAssignment(string name, Dictionary<Variable, IPExpr> dic)
     {
         var postfix = $"{string.Join("__", Dic2StrDic(dic).ToList().Select(p => $"{p.Key}_{p.Value}"))}";
@@ -45,26 +120,29 @@ public abstract class ParamAssignment
         for (var i = 0; i < indexArr.Length; i++)
         {
             indexArr[i] = (indexArr[i].Item1, indexArr[i].Item2 + 1);
-            // Console.WriteLine($"globalParams[globalVariables[{i}].Name].Count = {globalParams[globalVariables[i].Name].Count}");
             if (indexArr[i].Item2 < globalParams[indexArr[i].Item1].Count) return true;
             indexArr[i] = (indexArr[i].Item1, 0);
         }
+
         return false;
     }
 
-    public static void IterateAssignments(SafetyTest safety, List<Variable> globalParams, Action<Dictionary<Variable, IPExpr>> f)
+    public static void IterateAssignments(SafetyTest safety, List<Variable> globalParams,
+        Action<Dictionary<Variable, IPExpr>> f)
     {
-        // Console.WriteLine($"safety.ParamExpr.Count = {safety.ParamExpr.Count}");
-        var indexArr = safety.ParamExprMap.ToList().Zip(Enumerable.Repeat(0, safety.ParamExprMap.Count), (x, y) => (x.Key, y)).ToArray();
+        var indexArr = safety.ParamExprMap.ToList()
+            .Zip(Enumerable.Repeat(0, safety.ParamExprMap.Count), (x, y) => (x.Key, y)).ToArray();
+        var universe = safety.ParamExprMap.ToDictionary(kv => kv.Key, kv => kv.Value.Count);
+        var assignmentIndices = new List<Dictionary<string, int>>();
         do
         {
             var indexDic = indexArr.ToDictionary(item => item.Item1, item => item.Item2);
             var dic = IndexDic2Dic(globalParams, safety.ParamExprMap, indexDic);
-            // Console.WriteLine($"{string.Join(',', dic.ToList())} |- {safety.AssumeExpr}");
-            // Console.WriteLine($"{string.Join(',', dic.ToList())} |- {safety.AssumeExpr} = {ForceBool(Eval(dic, safety.AssumeExpr))}");
             if (!SimpleExprEval.ForceBool(SimpleExprEval.Eval(dic, safety.AssumeExpr))) continue;
-            // Console.WriteLine($"indexArr: {string.Join(',', indexArr)}");
-            f(dic);
+            assignmentIndices.Add(indexDic);
         } while (Next(indexArr, safety.ParamExprMap));
+
+        foreach (var i in GreedyCoverageExplore(universe, assignmentIndices, safety.Twise))
+            f(IndexDic2Dic(globalParams, safety.ParamExprMap, assignmentIndices[i]));
     }
 }

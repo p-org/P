41108d8d0e73ed2f58089216ccd911d3ae07f10d
Aishwarya Jagarapu <aishuj@amazon.com>
2025-05-12 11:03:52 -0700

    fix tutorials.yml

--- Src/Scripts/TutorialsChecker/compile.sh
+++ Src/Scripts/TutorialsChecker/compile.sh
@@ -18,8 +18,7 @@ for folder in $folders; do
     echo "------------------------------------------------------"
     echo "Compiling $folder!"
     echo "------------------------------------------------------"
-    alias pl='dotnet /Bld/Drops/Release/Binaries/net8.0/p.dll'
-    pl compile
+    dotnet /Bld/Drops/Release/Binaries/net8.0/p.dll compile
 
     # Check and print any errors
     if [ $? -ne 0 ]; then
